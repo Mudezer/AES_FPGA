9133f2dd4132c1123ad1ccdc3cca3687809509a8
Mudezer <loic.bermudez@gmail.com>
2023-12-16 12:27:32 +0100

    SubBytes optimised

--- Vivado_projects/SubBytes/SubBytes.sim/sim_1/behav/xsim/xsim.dir/SubBytes_tb_behav/obj/xsim_1.c
+++ Vivado_projects/SubBytes/SubBytes.sim/sim_1/behav/xsim/xsim.dir/SubBytes_tb_behav/obj/xsim_1.c
@@ -54,19 +54,17 @@
 #endif
 typedef void (*funcp)(char *, char *);
 extern int main(int, char**);
-IKI_DLLESPEC extern void execute_47(char*, char *);
-IKI_DLLESPEC extern void execute_46(char*, char *);
-IKI_DLLESPEC extern void execute_15(char*, char *);
-IKI_DLLESPEC extern void transaction_0(char*, char*, unsigned, unsigned, unsigned);
+IKI_DLLESPEC extern void execute_129(char*, char *);
+IKI_DLLESPEC extern void execute_14(char*, char *);
 IKI_DLLESPEC extern void vhdl_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
-funcp funcTab[5] = {(funcp)execute_47, (funcp)execute_46, (funcp)execute_15, (funcp)transaction_0, (funcp)vhdl_transfunc_eventcallback};
-const int NumRelocateId= 5;
+funcp funcTab[3] = {(funcp)execute_129, (funcp)execute_14, (funcp)vhdl_transfunc_eventcallback};
+const int NumRelocateId= 3;
 
 void relocate(char *dp)
 {
-	iki_relocate(dp, "xsim.dir/SubBytes_tb_behav/xsim.reloc",  (void **)funcTab, 5);
-	iki_vhdl_file_variable_register(dp + 11584);
-	iki_vhdl_file_variable_register(dp + 11640);
+	iki_relocate(dp, "xsim.dir/SubBytes_tb_behav/xsim.reloc",  (void **)funcTab, 3);
+	iki_vhdl_file_variable_register(dp + 4464);
+	iki_vhdl_file_variable_register(dp + 4520);
 
 
 	/*Populate the transaction function pointer field in the whole net structure */

dcb1bdbf484e5eda0484113e6a494a820aa6c28b
Mudezer <loic.bermudez@gmail.com>
2023-12-18 09:28:30 +0100

    9 rounds state machine working

--- implementation/noClock/Core.vhd
+++ implementation/noClock/Core.vhd
@@ -35,7 +35,9 @@ component MixColumns is port(
     output : out matrix
 ); end component;
 
-type statetype is (c0, c1, c2, c3, c4, c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16,c17,c18,c19,c20,c21,c22,c23,c24,c25,c26,c27,c28,c29,c30,c31,c32,c33,c34,c35,c36);
+type statetype is (c0,c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12
+    ,c13,c14,c15,c16,c17,c18,c19,c20,c21,c22,c23,c24,c25,c26,c27,c28
+    ,c29,c30,c31,c32,c33,c34,c35,c36);
 signal currentState, nextState : statetype;
 
 signal inputAdd, inputSub, inputShift, inputMix : matrix;
@@ -103,7 +105,7 @@ begin
             end if;
             when c8 => inputSub <= outputAdd; -- AddRoundKey => SubBytes (2nd round)
             if EN = '1' then
-                nextState <= c0;
+                nextState <= c9;
             else
                 nextState <= c8;
             end if;
